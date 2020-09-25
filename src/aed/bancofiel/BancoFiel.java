556f353e69359f6ba1233dc95ec75bfc992e1255
aafrecct <aafrected@gmail.com>
2020-09-25 10:49:30 +0200

    Bancofiel files

---
+++ src/aed/bancofiel/BancoFiel.java
@@ -0,0 +1,37 @@
+package aed.bancofiel;
+
+import java.util.Comparator;
+import es.upm.aedlib.indexedlist.IndexedList;
+import es.upm.aedlib.indexedlist.ArrayIndexedList;
+
+
+/**
+ * Implements the code for the bank application.
+ * Implements the client and the "gestor" interfaces.
+ */
+public class BancoFiel implements ClienteBanco, GestorBanco {
+
+  // NOTAD. No se deberia cambiar esta declaracion.
+  public IndexedList<Cuenta> cuentas;
+
+  // NOTAD. No se deberia cambiar esta constructor.
+  public BancoFiel() {
+    this.cuentas = new ArrayIndexedList<Cuenta>();
+  }
+
+  // ----------------------------------------------------------------------
+  // Anadir metodos aqui ...
+
+
+
+
+  // ----------------------------------------------------------------------
+  // NOTAD. No se deberia cambiar este metodo.
+  public String toString() {
+    return "banco";
+  }
+  
+}
+
+
+
