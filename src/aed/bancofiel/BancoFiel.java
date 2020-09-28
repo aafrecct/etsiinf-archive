f2b093623992f95669e2b25ff5c33bde309d3802
aafrecct <aafrected@gmail.com>
2020-09-28 11:11:03 +0200

    10

--- src/aed/bancofiel/BancoFiel.java
+++ src/aed/bancofiel/BancoFiel.java
@@ -48,7 +48,7 @@ public class BancoFiel implements ClienteBanco, GestorBanco {
   private static boolean isSorted(IndexedList<Cuenta> list, Comparator<Cuenta> cmp) {
     int i = 0;
     for ( ; i < list.size() - 1 && cmp.compare(list.get(i), list.get(i + 1)) < 0; i++) {}
-      return i < list.size() - 1;
+    return i < list.size() - 1;
     }
   
   //Metodo que busca de manera binaria un elemento en la lista
@@ -78,13 +78,13 @@ public class BancoFiel implements ClienteBanco, GestorBanco {
   
   @Override
   public IndexedList<Cuenta> getCuentasOrdenadas(Comparator<Cuenta> cmp) {
-    if (isSorted(cuentas, cmp)) {
-      return cuentas;
-    } else {
-      IndexedList<Cuenta> cuentas_new_sort = new ArrayIndexedList<Cuenta>((ArrayIndexedList<Cuenta>) cuentas);
-      quickSort(cuentas_new_sort, 0, cuentas_new_sort.size() - 1, cmp);
-      return cuentas_new_sort;
-    }
+//    if (isSorted(cuentas, cmp)) {
+//      return cuentas;
+//    } else {
+    IndexedList<Cuenta> cuentas_new_sort = new ArrayIndexedList<Cuenta>((ArrayIndexedList<Cuenta>) cuentas);
+    quickSort(cuentas_new_sort, 0, cuentas_new_sort.size() - 1, cmp);
+    return cuentas_new_sort;
+//    }
     
   }
 
@@ -94,11 +94,11 @@ public class BancoFiel implements ClienteBanco, GestorBanco {
     int i = 0;
     for ( ; i < cuentas.size() && !(cuentas.get(i).getId().compareTo(nueva_cuenta.getId()) < 0); i++) {}
     cuentas.add(i, nueva_cuenta);
-    System.out.println("Cuenta creada con el dni y saldo inicial : " + dni + " " + saldoIncial);
-    for(Cuenta c : cuentas) {
-    	System.out.println("id: " + c.getId());
-    }
-    	return nueva_cuenta.getId();
+    //System.out.println("Cuenta creada con el dni y saldo inicial : " + dni + " " + saldoIncial);
+    //for(Cuenta c : cuentas) {
+    //	System.out.println("id: " + c.getId());
+    //}
+    return nueva_cuenta.getId();
     
   }
 
@@ -137,50 +137,50 @@ public class BancoFiel implements ClienteBanco, GestorBanco {
     origen.retirar(cantidad);
     destino.ingresar(cantidad);
   }
-
-  @Override
-  public IndexedList<String> getIdCuentas(String dni) {
-	  IndexedList<String> sol = new ArrayIndexedList<String>();
-	  //Nuestro método binary search nos busca un elemento aleatorio dentro de el "bloque" de cuentas con el mismo dni
-	  int med = 0;
+  
+  private void getAccDni(int i, int dir, String dni, IndexedList<Cuenta> list) {
+    while(i < cuentas.size() && i >= 0 && cuentas.get(i).getDNI().equals(dni)) {
+      int pos = dir > 0 ? list.size() : 0;
+      list.add(pos, cuentas.get(i));
+      i += dir;
+    }
+  }
+  
+  private  IndexedList<Cuenta> getCuentasByDNI(String dni) {
+    IndexedList<Cuenta> sol = new ArrayIndexedList<Cuenta>();
+    //Nuestro método binary search nos busca un elemento aleatorio dentro de el "bloque" de cuentas con el mismo dni
+    int med = 0;
     try {
       med = binarySearch(cuentas, 0, cuentas.size(), dni);
     } catch (CuentaNoExisteExc e) {}
-	  //Por ello hacemos una busqueda por debajo y por arriba para poder tener todas las cuentas con el mismo id
-	  int i = med + 1;
-	  int j = med - 1;
-	  
-	  while( i < cuentas.size() && cuentas.get(i).getDNI().equals(dni)) {
-		  //añade a solucion por la parte de detras los elementos que van despues de med
-		  sol.add(sol.size(), cuentas.get(i).getDNI());
-		  i++;
-	  }
-	  while( j > 0 && cuentas.get(j).getDNI().equals(dni)) {
-		  //añade por la perte de delante los elementos que van antes de med
-		  sol.add(0, cuentas.get(j).getDNI());
-		  j--;
-	  }
-	//Devuelve un array ordenado por id de las cuentas con el mismo dni  
+    
+    sol.add(0, cuentas.get(med));
+    getAccDni(med + 1, 1, dni, sol);
+    getAccDni(med - 1, -1, dni, sol);
+    for(Cuenta c : sol) {
+      System.out.println(c.getId());
+    }
+    
+  //Devuelve un array ordenado por id de las cuentas con el mismo dni  
+    return sol;
+  }
+  
+  @Override
+  public IndexedList<String> getIdCuentas(String dni) {
+    IndexedList<String> sol = new ArrayIndexedList<String>();
+    for (Cuenta c : getCuentasByDNI(dni)) {
+      sol.add(sol.size(), c.getId());
+    }
     return sol;
   }
 
   @Override
   public int getSaldoCuentas(String dni) {
-  	  IndexedList<String> list = new ArrayIndexedList<String>();
-  	  if (list.size() == 0) {
-  	    return 0;
-  	  } else {
-  	    list = getIdCuentas(dni);
-  	    int primera_cuenta = 0;
-  	    try {
-  	      primera_cuenta = binarySearch(cuentas, 0, cuentas.size(), list.get(0));
-  	    } catch (CuentaNoExisteExc e) {}
-        int sum = 0;
-        for(int i = primera_cuenta; i < primera_cuenta + list.size(); i++) {
-          sum += cuentas.get(i).getSaldo();
-        }
-        return sum;
-  	  }
+    int sum = 0;
+    for (Cuenta c : getCuentasByDNI(dni)) {
+      sum += c.getSaldo();
+    }
+    return sum;
   }
 
   // ----------------------------------------------------------------------
@@ -188,7 +188,6 @@ public class BancoFiel implements ClienteBanco, GestorBanco {
   public String toString() {
     return "banco";
   }
-  
 }
 
 
