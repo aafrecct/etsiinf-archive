38ac13357fc859baf2701e86e360b4b4f6d66f8f
aafrecct <aafrected@gmail.com>
2020-10-05 00:20:35 +0200

    Three methods left [lab2]

--- src/aed/multisets/MultiSetList.java
+++ src/aed/multisets/MultiSetList.java
@@ -23,49 +23,102 @@ public class MultiSetList<Element> implements MultiSet<Element> {
      * Constructs an empty multiset.
      */
     public MultiSetList() {
-	this.elements = new NodePositionList<Pair<Element,Integer>>();
+      this.elements = new NodePositionList<Pair<Element,Integer>>();
     }
 
-
+    private boolean equals(Element elem1, Element elem2) {
+      if (elem1 == null) {
+        return elem2 == null;
+      } else {
+        return elem1.equals(elem2);
+      }
+    }
+    
+    private Position<Pair<Element, Integer>> getPosition(Element elem){
+      if (elements.size() == 0) {
+        // If the size of the list is 0, return a null object.
+        return null;
+      } else {
+        Position<Pair<Element,Integer>> pos = elements.first();
+        boolean found = false;
+        while (!found && pos != elements.last()) {
+          if (equals(pos.element().getLeft(), elem)) {
+            found = true;
+          } else {
+            pos = elements.next(pos);
+          }
+        }
+        return equals(pos.element().getLeft(), elem) ? pos : null;
+      }
+    }
+    
     @Override
     public void add(Element elem, int n) {
-      // TODO Auto-generated method stub
-      
+      System.out.println(elem);
+      System.out.println(n);
+      Position<Pair<Element, Integer>> pos = getPosition(elem);
+      System.out.println("pos: " + pos);
+      if (n > 0) {
+        if (pos != null) {
+          pos.element().setRight(pos.element().getRight() + n);
+        } else {
+          elements.addLast(new Pair<Element, Integer>(elem, n));
+          System.out.println("pos: " + elements.last());
+        }
+        size += n;
+      } else if (n == 0) {
+        // Literally do nothing.
+      } else {
+        throw new IllegalArgumentException("El numero de elementos especificado esta fuera del rango de valores esperados.");
+      }
+      System.out.println(elements);
+      System.out.println("---");
     }
 
 
     @Override
     public void remove(Element elem, int n) {
-      // TODO Auto-generated method stub
-      
+      Position<Pair<Element, Integer>> pos = getPosition(elem);
+      if (pos == null || n < 0 || n > pos.element().getRight()) {
+        throw new IllegalArgumentException("El numero de elementos especificado esta fuera del rango de valores esperados.");
+      } else {
+        pos.element().setRight(pos.element().getRight() - n);
+        if (pos.element().getRight() == 0) {
+          elements.remove(pos);
+        }
+        size -= n;
+      }
     }
 
 
     @Override
     public int count(Element elem) {
-      // TODO Auto-generated method stub
-      return 0;
+      Position<Pair<Element, Integer>> pos = getPosition(elem);
+      return pos == null ? 0 : pos.element().getRight();
     }
 
 
     @Override
     public int size() {
-      // TODO Auto-generated method stub
-      return 0;
+      return size;
     }
 
 
     @Override
     public boolean isEmpty() {
-      // TODO Auto-generated method stub
-      return false;
+      return size == 0;
     }
 
 
     @Override
     public PositionList<Element> allElements() {
-      // TODO Auto-generated method stub
-      return null;
+      PositionList<Element> list = new NodePositionList<Element>();
+      for (Pair<Element,Integer> e : elements) {
+        for (int i = 0; i < e.getRight(); i++) {
+          list.addLast(e.getLeft());
+        }
+      }
+      return list;
     }
 
 
