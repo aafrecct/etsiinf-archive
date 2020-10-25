43414c7d1bcd735e14145bc4675006539e958d4a
aafrecct <aafrected@gmail.com>
2020-10-25 17:12:03 +0100

    El pato Donald

--- src/aed/recursion/Crypto.java
+++ src/aed/recursion/Crypto.java
@@ -10,40 +10,60 @@ public class Crypto {
   
   private static Integer encryptChar (PositionList<Character> key, Character character,
                                       Integer result, Position<Character> keyPos) {
-    if (keyPos.element().equals(character)) {
+    if (keyPos.element().equals(character)) {    // Character is found.
       return result;
-    } else if (keyPos.element().compareTo(character) < 0) {
+    } else if (keyPos.element().compareTo(character) < 0) {    // Character is left of target.
       keyPos = key.next(keyPos);
       return encryptChar(key, character, result + 1, keyPos);
-    } else {
-      
+    } else {    // Character is right of target. 
       return encryptChar(key, character, result - 1, keyPos);
     }
   }
   
   private static PositionList<Integer> encrypt (PositionList<Character> key, PositionList<Character> text, 
-                                                PositionList<Integer> result, Position<Character> keyPos, 
-                                                Position<Character> characterPos, Position<Integer> resultPos) {
-    if (characterPos == null) {
+                                                PositionList<Integer> result, Position<Character> characterPos,
+                                                int prev_num) {
+    if (characterPos == null) {    // Reached end of string.
       return result;
-    } else {    
-      result.set(resultPos, encryptChar(key, characterPos.element(), 0, keyPos));
-      return encrypt(key, text, result, keyPos, text.next(characterPos), result.next(resultPos));
+    } else {
+      // Encrypt one character.
+      Integer last_num = encryptChar(key, characterPos.element(), 0, key.first());
+      result.addLast(last_num - prev_num);
+      // Call function with next character.
+      return encrypt(key, text, result, text.next(characterPos), last_num);
     }
   }
   
   public static PositionList<Integer> encrypt (PositionList<Character> key, PositionList<Character> text) {
     PositionList<Integer> result = new NodePositionList<Integer>();
-    return encrypt(key, text, result, key.first(), text.first(), result.first());
+    return encrypt(key, text, result, text.first(), 0);
   }
   
+  public static Character get (PositionList<Character> key, int pos, Position<Character> keyPos) {
+    if (pos == 0) {
+      return keyPos.element();
+    } else {
+      keyPos = key.next(keyPos);
+      return get(key, pos - 1, keyPos);
+    }
+  }
+  
+  
   public static PositionList<Character> decrypt (PositionList<Character> key, PositionList<Integer> encodedText,
-                                                 PositionList<Character> result, Position<Character> keyPos) {
-    return null;
+                                                 PositionList<Character> result, Position<Integer> characterPos,
+                                                 int last_pos) {
+    if (characterPos == null) {
+      return result;
+    } else {
+      int pos = last_pos + characterPos.element();
+      result.addLast(get(key, pos, key.first()));
+      return decrypt(key, encodedText, result, encodedText.next(characterPos), pos);
+    }
   }
   
   public static PositionList<Character> decrypt (PositionList<Character> key, PositionList<Integer> encodedText) {
-    return null;
+    PositionList<Character> result = new NodePositionList<Character>();
+    return decrypt(key, encodedText, result, encodedText.first(), 0);
   }
 
 }
