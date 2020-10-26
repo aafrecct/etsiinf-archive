f75f9a5b70a5b51118fef4face61de5d8c83319a
Antonio Carpintero Castilla <karpin@abyss.local>
2020-10-26 15:31:23 +0100

    arreglado el algoritmo, solo queda ver porque hace stackoverflow

--- src/aed/recursion/Explorador.java
+++ src/aed/recursion/Explorador.java
@@ -17,21 +17,15 @@ public class Explorador {
 		} else {
 			//No esta el tesoro asi que tenemos que ir al siguiente que no este marcado con tiza
 			Lugar next = nextCamino(lugar);
-
+			lugar.marcaSueloConTiza();
 			if(next == null) { //No hay siguiente asi que tenemos que volver al anterior
 				
-				if(lugar.sueloMarcadoConTiza()) { //si este estaba marcado es que habiamos pasado asi que hay que quitarlo de la pila
-					
-					camino.remove(camino.last());
-				} else { //si no estaba marcado, no hace falta sacarlo de la pila porque no esta en un principio
-					lugar.marcaSueloConTiza();
-				}
+				camino.remove(camino.last());
 				return exploraR(camino.last().element(), camino);
 				
 			} else {
-				//metemos este lugar en la pila y llamamos a la unfion en el siguiente
-				camino.addLast(lugar);
-				lugar.marcaSueloConTiza();
+				//metemos este lugar en la pila y llamamos a la funcion en el siguiente
+				camino.addLast(next);
 				return exploraR(next,camino);
 			}
 		}
@@ -41,15 +35,15 @@ public class Explorador {
 	private static Lugar nextCamino(Lugar lugar) {
 		Iterator<Lugar> it = lugar.caminos().iterator();
 		boolean found = false;
-		Lugar current = null;
+		Lugar next = null;
 		
 		while(it.hasNext() && !found) {
-			current  = it.next();
-			if(!current.sueloMarcadoConTiza()) {
+			next  = it.next();
+			if(!next.sueloMarcadoConTiza()) {
 				found = true;
 			}
 		}
-		return current;
+		return next;
 	}
 		
   
