e1a159cfdfe032469587b418e7f2a48f9e2f2ead
aafrecct <aafrected@gmail.com>
2020-11-21 11:14:57 +0100

    Metodo 1!

--- src/aed/treesearch/TreeSearch.java
+++ src/aed/treesearch/TreeSearch.java
@@ -8,8 +8,51 @@ import es.upm.aedlib.tree.*;
 
 
 public class TreeSearch {
+  
+  private static boolean match (Position<String> expr, Position<String> node) {
+    if (expr.element().equals("*")) {
+      return true;
+    } else {
+      return expr.element().equals(node.element());
+    }
+  }
+  
+  private static Set<Position<String>> search (Tree<String> t, PositionList<String> searchExprs, Position<String> node,
+                                               Set<Position<String>> result, PositionList<String> streak, Position<String> searchPos) {
+    if (t.isInternal(node)) {
+      if (match(searchPos, node)) {
+        if (streak.size() + 1 == searchExprs.size()) {
+          result.add(node);
+          streak = new NodePositionList<String>();
+          searchPos = searchExprs.first();
+        } else {
+          streak.addLast(node.element());
+          searchPos = searchExprs.next(searchPos);
+        }
+      } else {
+        streak = new NodePositionList<String>();
+        searchPos = searchExprs.first();
+        if (match(searchPos, node)) {
+          streak.addLast(node.element());
+          searchPos = searchExprs.next(searchPos);
+        }
+      }
+      for (Position<String> child : t.children(node)) {
+        result = search(t, searchExprs, child, result, streak, searchPos);
+      }
+      return search(t, searchExprs, node, result, streak, searchPos);
+    } else {
+      if (streak.size() + 1 == searchExprs.size()) {
+        if (match(searchExprs.last(), node)) {
+          result.add(node);
+        }
+      }
+    }
+    return result;
+  }
+  
   public static Set<Position<String>> search(Tree<String> t,
-					     PositionList<String> searchExprs) {
+                                             PositionList<String> searchExprs) {
     return null;
   }
 
