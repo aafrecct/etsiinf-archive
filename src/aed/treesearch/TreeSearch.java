23f209112590d861726af62ce31dd1b8b528b8f1
Antonio Carpintero Castilla <karpin@abyss.local>
2020-11-21 17:02:08 +0100

    lo tengo

--- src/aed/treesearch/TreeSearch.java
+++ src/aed/treesearch/TreeSearch.java
@@ -1,6 +1,8 @@
 package aed.treesearch;
 
 
+import java.util.Iterator;
+
 import es.upm.aedlib.Position;
 import es.upm.aedlib.set.*;
 import es.upm.aedlib.positionlist.*;
@@ -33,4 +35,49 @@ public class TreeSearch {
   public static Set<Position<String>> search(Tree<String> t, PositionList<String> searchExprs) {
     return search(t, searchExprs, searchExprs.first(), t.root(), new HashTableMapSet<Position<String>>());
   }
+  
+  public static Tree<String> constructDeterministicTree(Set<PositionList<String>> paths) {
+		LinkedGeneralTree<String> t = new LinkedGeneralTree<String>();
+		
+		//hacemos una copia para no andar molestando
+		Set<PositionList<String>> ListOfPaths = new HashTableMapSet<PositionList<String>>();
+		for(PositionList<String> elem : paths) {
+			ListOfPaths.add(elem);
+		}
+		
+		for(PositionList<String> path : ListOfPaths) {
+			
+			  //primer caso con el arbol vacio para meter la root
+			  if(t.isEmpty()) {
+				  t.addRoot(path.first().element());
+			  }
+			  //Procedemos sabiendo que hay una root ya y quitamos el primer elemento(que es la root que ya tenemos)
+			  path.remove(path.first());
+			  Position<String> cursor = t.root();
+			  //Miramos para cada elemento de la position list si es hijo, luego el cursor pasa a ser ese si o si
+			  for(String name : path) {
+				  if(isChild(t, cursor, name) == null) {
+					  cursor = t.addChildLast(cursor, name);
+				  }else {
+					  cursor = isChild(t, cursor, name);
+				  }
+			  }
+		}
+	    return t;
+	  }
+  
+  //Metodo que devuelve una posicion si hay un hijo con ese string o null si no lo hay
+  private static Position<String> isChild(Tree<String> t, Position<String> father, String name) {
+	  //Una simple busqueda con iteradores
+	  Position<String> boy = null;
+	  Iterator<Position<String>> it = t.children(father).iterator(); 
+	  while(boy == null && it.hasNext()) {
+		  Position<String> n = it.next();
+		  if(n.element().equals(name)) {
+			  boy = n;
+		  }
+	  }
+	  return boy;
+  }
+
 }
