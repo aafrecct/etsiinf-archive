7a9bc4cfbd0bc6278357b232ace28d763926f359
aafrecct <aafrected@gmail.com>
2020-11-21 13:20:33 +0100

    Search Done

--- src/aed/treesearch/TreeSearch.java
+++ src/aed/treesearch/TreeSearch.java
@@ -9,51 +9,30 @@ import es.upm.aedlib.tree.*;
 
 public class TreeSearch {
   
-  private static boolean match (Position<String> expr, Position<String> node) {
-    if (expr.element().equals("*")) {
+  private static boolean match (String expr, Position<String> node) {
+    if (expr.equals("*")) {
       return true;
     } else {
-      return expr.element().equals(node.element());
+      return expr.equals(node.element());
     }
   }
   
-  private static Set<Position<String>> search (Tree<String> t, PositionList<String> searchExprs, Position<String> node,
-                                               Set<Position<String>> result, PositionList<String> streak, Position<String> searchPos) {
-    if (t.isInternal(node)) {
-      if (match(searchPos, node)) {
-        if (streak.size() + 1 == searchExprs.size()) {
-          result.add(node);
-          streak = new NodePositionList<String>();
-          searchPos = searchExprs.first();
-        } else {
-          streak.addLast(node.element());
-          searchPos = searchExprs.next(searchPos);
-        }
+  private static Set<Position<String>> search (Tree<String> t, PositionList<String> searchExprs, Position<String> expElem,
+                                               Position<String> node, Set<Position<String>> result) {
+    if (expElem != null && match(expElem.element(), node)) {
+      if (expElem.equals(searchExprs.last())) {
+        result.add(node);
       } else {
-        streak = new NodePositionList<String>();
-        searchPos = searchExprs.first();
-        if (match(searchPos, node)) {
-          streak.addLast(node.element());
-          searchPos = searchExprs.next(searchPos);
-        }
-      }
-      for (Position<String> child : t.children(node)) {
-        result = search(t, searchExprs, child, result, streak, searchPos);
-      }
-      return search(t, searchExprs, node, result, streak, searchPos);
-    } else {
-      if (streak.size() + 1 == searchExprs.size()) {
-        if (match(searchExprs.last(), node)) {
-          result.add(node);
+        for (Position<String> child : t.children(node)) {
+          search(t, searchExprs, searchExprs.next(expElem), child, result);
         }
       }
     }
     return result;
   }
   
-  public static Set<Position<String>> search(Tree<String> t,
-                                             PositionList<String> searchExprs) {
-    return null;
+  public static Set<Position<String>> search(Tree<String> t, PositionList<String> searchExprs) {
+    return search(t, searchExprs, searchExprs.first(), t.root(), new HashTableMapSet<Position<String>>());
   }
 
   public static Tree<String> constructDeterministicTree(Set<PositionList<String>> paths) {
