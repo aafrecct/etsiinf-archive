86a45d5a9f62b1350b0c4764601eed53523430ee
MR-KARPIN <anto.carpintero@gmail.com>
2020-11-29 16:16:06 +0100

    aniadido los 3 metodos sencillos, revisalos porsiaca

--- src/aed/airport/IncomingFlightsRegistry.java
+++ src/aed/airport/IncomingFlightsRegistry.java
@@ -1,8 +1,12 @@
 package aed.airport;
 
 
+import java.util.Iterator;
 import es.upm.aedlib.Entry;
 import es.upm.aedlib.Pair;
+import es.upm.aedlib.Position;
+import es.upm.aedlib.indexedlist.ArrayIndexedList;
+import es.upm.aedlib.indexedlist.IndexedList;
 import es.upm.aedlib.priorityqueue.*;
 import es.upm.aedlib.map.*;
 import es.upm.aedlib.positionlist.*;
@@ -12,8 +16,7 @@ import es.upm.aedlib.positionlist.*;
  * A registry which organizes information on airplane arrivals.
  */
 public class IncomingFlightsRegistry {
-
-
+	static private PositionList<Pair<String,Long>> flightsList = new NodePositionList<Pair<String,Long>>(); 
   /**
    * Constructs an class instance.
    */
@@ -24,12 +27,31 @@ public class IncomingFlightsRegistry {
    * A flight is predicted to arrive at an arrival time (in seconds).
    */
   public void arrivesAt(String flight, long time) {
+	  Position<Pair<String,Long>> elem = search(flight);
+	  if(elem == null) {
+		  flightsList.addLast(new Pair<String,Long>(flight,time));
+	  }else {
+		  elem.element().setRight(time);
+	  }
   }
+  
+  //Method that returns the element searched or null if it isnt on the list
+  private Position<Pair<String,Long>> search (String name){
+	  Position<Pair<String,Long>> cursor = flightsList.first();
+	  while(cursor != null && cursor.element().getLeft() != name) {
+			  cursor = flightsList.next(cursor);
+	  }
+	  return cursor;
+	  
+  }
+  
 
   /**
    * A flight has been diverted, i.e., will not arrive at the airport.
    */
   public void flightDiverted(String flight) {
+	  if(search(flight) != null)
+		  flightsList.remove(search(flight));
   }
 
   /**
@@ -38,7 +60,7 @@ public class IncomingFlightsRegistry {
    * to arrive.
    */
   public Long arrivalTime(String flight) {
-    return null;
+    return search(flight) == null? null : search(flight).element().getRight();
   }
 
   /**
