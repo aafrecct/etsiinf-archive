32a556769e1609230e79e97cd59806ebdb27b2d6
MR-KARPIN <pxpshack@gmail.com>
2020-12-13 13:58:12 +0100

    hice lo mio de manera mia

--- src/aed/delivery/Delivery.java
+++ src/aed/delivery/Delivery.java
@@ -1,7 +1,9 @@
 package aed.delivery;
 
 import es.upm.aedlib.positionlist.PositionList;
+import es.upm.aedlib.Entry;
 import es.upm.aedlib.Position;
+import es.upm.aedlib.map.Map;
 import es.upm.aedlib.positionlist.NodePositionList;
 import es.upm.aedlib.graph.DirectedGraph;
 import es.upm.aedlib.graph.DirectedAdjacencyListGraph;
@@ -41,8 +43,59 @@ public class Delivery<V> {
   // The list containts a series of vertices, with no repetitions (even if the path
   // can be expanded to a cycle).
   public PositionList <Vertex<V>> tour() {
-    return null;
-  }
+		Map<Vertex<V>, V> path = new HashTableMap<Vertex<V>,V>();
+		PositionList<Vertex<V>> sol = new NodePositionList<Vertex<V>>();
+		for(Entry<Vertex<V>, V> x : tourRec(towns.vertices(), path).entries()) {
+			sol.addLast(x.getKey());
+		}
+	    return sol;
+	  }
+	  
+	  //En la primera vuelta, por cada vertice en el grafo va a llamar a los hijos de esos vertices que 
+	  //a su vez van a llamar a sus hijos y etc etc, el metodo para cuando ya no tengan hijos los vertices,
+	  // cuando al añadir un hijo este ya dentro o cuando se encuentra
+	  
+	  private boolean foundTourRec = false;
+	  private Map<Vertex<V>,V> tourRec(Iterable<Vertex<V>> iter, Map<Vertex<V>,V> path) {
+		  
+		  //Si ya esta encontrado no entramos en el metodo
+		  if(foundTourRec) {
+			  return path;
+		  }
+		  //Comprobacion de que estan todos dentro del camino y lo ha encontrado
+		  if(path.size() == towns.numVertices()) {
+			  this.foundTourRec = true;
+			  return path;
+		  }
+		  // Comprobacion de que no hay mas hijos que visitar
+		  if(!iter.iterator().hasNext()) {
+				return null;
+		  }
+		  Map<Vertex<V>,V> sol = new HashTableMap<Vertex<V>, V>();
+		  Map<Vertex<V>,V> copy = new HashTableMap<Vertex<V>, V>(path);
+		  for( Vertex<V> vertex : iter) {
+			//Si el put devuelve null es que no estaba en el camino y seguimos por ahi
+			//En otro caso seguimos 
+			if(path.put(vertex, vertex.element()) == null && !foundTourRec) {
+				sol = tourRec(iterableVertex(vertex), path);
+				path = copy;
+				
+			}
+		  }
+		   /*Si ha pasado que no esta vacio, es distinto el numero de elementos en el camino y el grafo,
+		   * Todavia no esta encontrado, y por cada elemento estan repetidos eso significa que no hay camino posible
+		   * asi que devolvemos null*/
+		  return sol;
+		
+	}
+	  //Método que devuelve un iterable de todos los nodos hijos de dado un cierto nodo padre
+	  private Iterable<Vertex<V>> iterableVertex (Vertex<V> vertex){
+		  PositionList<Vertex<V>> list = new NodePositionList<Vertex<V>>();
+		  for(Edge<Integer> edge : towns.outgoingEdges(vertex)) {
+			  list.addLast(towns.endVertex(edge));
+		  }
+		  return list;
+	  }
 
   public int length(PositionList<Vertex<V>> path) {
     return 0;
