749668c6700a674d5c6fe74ba023eb2531c6692f
Álvaro Ferrero <alfedi3867@hotmail.com>
2023-05-28 18:03:15 +0200

    feat(Repo): Added User model operations with DB

--- lib/bet_unfair/repo.ex
+++ lib/bet_unfair/repo.ex
@@ -1,5 +1,44 @@
 defmodule BetUnfair.Repo do
+  alias BetUnfair.Models
+
   use Ecto.Repo,
     otp_app: :bet_unfair,
     adapter: Ecto.Adapters.Postgres
+
+  def get_user(id) do
+    case Models.User
+         |> BetUnfair.Repo.get_by(id: id) do
+      nil ->
+        {:ok, %{}}
+
+      user ->
+        {:ok, user}
+    end
+  end
+
+  def add_user(user) do
+    case user |> BetUnfair.Repo.insert() do
+      {:ok, _} = res -> res
+      _ -> {:error, {:error_add_user, "User cannot be inserted in the DB"}}
+    end
+  end
+
+  def edit_user(user) do
+    case user |> BetUnfair.Repo.update() do
+      {:ok, _} = res -> res
+      _ -> {:error, {:error_edit_user, "User cannot be edited in the DB"}}
+    end
+  end
+
+  def get_all_users() do
+    # Even if there are none it will return an empty map
+    {:ok, Models.User |> BetUnfair.Repo.all()}
+  end
+
+  def delete_user(user) do
+    case user |> BetUnfair.Repo.delete() do
+      {:ok, _} = res -> res
+      _ -> {:error, {:error_delete_user, "User cannot be deleted"}}
+    end
+  end
 end
