62a554a4f8bb5de6ffe16c90b2bcb2fb34bdac4a
Álvaro Ferrero <alfedi3867@hotmail.com>
2023-05-28 19:24:08 +0200

    feat(Repo): Add Models.Bet Repo operations

--- lib/bet_unfair/repo.ex
+++ lib/bet_unfair/repo.ex
@@ -5,6 +5,8 @@ defmodule BetUnfair.Repo do
     otp_app: :bet_unfair,
     adapter: Ecto.Adapters.Postgres
 
+  # User operations
+  # ===============
   def get_user(id) do
     case Models.User
          |> BetUnfair.Repo.get_by(id: id) do
@@ -39,6 +41,46 @@ defmodule BetUnfair.Repo do
     case user |> BetUnfair.Repo.delete() do
       {:ok, _} = res -> res
       _ -> {:error, {:error_delete_user, "User cannot be deleted"}}
+    end
+  end
+
+  # Bet operations
+  # ==============
+  def get_bet(id) do
+    case Models.Bet |> BetUnfair.Repo.get_by(user: id) do
+      nil ->
+        {:ok, %{}}
+
+      bet ->
+        {:ok, bet}
+    end
+  end
+
+  def add_bet(bet) do
+    case BetUnfair.Repo.insert(bet) do
+      {:ok, _} = res -> res
+      _ -> {:error, {:error_add_bet, "Bet cannot be inserted in the DB"}}
+    end
+  end
+
+  def edit_bet(bet) do
+    case BetUnfair.Repo.update(bet) do
+      {:ok, _} = res ->
+        res
+
+      _ ->
+        {:error, {:error_edit_bet, "Bet cannot be edited in the DB"}}
+    end
+  end
+
+  def get_all_bets() do
+    {:ok, Models.Bet |> BetUnfair.Repo.all()}
+  end
+
+  def delete_bet(bet) do
+    case BetUnfair.Repo.delete(bet) do
+      {:ok, _} = res -> res
+      _ -> {:error, {:error_delete_bet, "Bet cannot be deleted"}}
     end
   end
 end
