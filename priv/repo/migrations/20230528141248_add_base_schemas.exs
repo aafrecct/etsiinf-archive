57d5bc4f864f8172cfffa58ca6b021018a30a8c7
Álvaro Ferrero <alfedi3867@hotmail.com>
2023-05-29 19:33:20 +0200

    fix(migrations): Fixed migrations database. Now works as expected

--- priv/repo/migrations/20230528141248_add_base_schemas.exs
+++ priv/repo/migrations/20230528141248_add_base_schemas.exs
@@ -2,22 +2,10 @@ defmodule BetUnfair.Repo.Migrations.AddBaseSchemas do
   use Ecto.Migration
 
   def change do
-    create table(:bets) do
-      add :bet_type, :string
-      add :user, :string
-      add :market, :integer
-      add :original_stake, :integer
-      add :remaining_stake, :integer
-      add :odds, :integer
-      add :status, :string
-      add :matched, references("bets")
-    end
-
     create table(:users, primary_key: false) do
       add :id, :string, primary_key: true
       add :name, :string
       add :balance, :integer
-      add :bets, references("bets")
     end
 
     create table(:markets) do
@@ -25,7 +13,17 @@ defmodule BetUnfair.Repo.Migrations.AddBaseSchemas do
       add :description, :string
       add :status, :string
       add :result, :boolean, default: false
-      add :bets, references("bets")
+    end
+
+    create table(:bets) do
+      add :bet_type, :string
+      add :user, references(:users, type: :string, name: "user")
+      add :market, references(:markets, name: "market")
+      add :original_stake, :integer
+      add :remaining_stake, :integer
+      add :odds, :integer
+      add :status, :string
+      add :matched, references(:bets, name: "matched")
     end
   end
 end
