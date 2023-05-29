a1cbbcfe762e597c5488b18bbf88ff8aab476da4
Álvaro Ferrero <alfedi3867@hotmail.com>
2023-05-29 13:19:00 +0200

    feat(migrations): Add DB tables migrations

--- priv/repo/migrations/20230528141248_add_base_schemas.exs
+++ priv/repo/migrations/20230528141248_add_base_schemas.exs
@@ -2,6 +2,30 @@ defmodule BetUnfair.Repo.Migrations.AddBaseSchemas do
   use Ecto.Migration
 
   def change do
+    create table(:bets) do
+      add :bet_type, :string
+      add :user, :string
+      add :market, :integer
+      add :original_stake, :integer
+      add :remaining_stake, :integer
+      add :odds, :integer
+      add :status, :string
+      add :matched, references("bets")
+    end
 
+    create table(:users, primary_key: false) do
+      add :id, :string, primary_key: true
+      add :name, :string
+      add :balance, :integer
+      add :bets, references("bets")
+    end
+
+    create table(:markets) do
+      add :name, :string
+      add :description, :string
+      add :status, :string
+      add :result, :boolean, default: false
+      add :bets, references("bets")
+    end
   end
 end
