805bc3c42fff4c7cd893511daebae200c123eb7f
Álvaro Ferrero <alfedi3867@hotmail.com>
2023-05-29 23:34:08 +0200

    feat(Repo): Added market DB methods

--- lib/bet_unfair/repo.ex
+++ lib/bet_unfair/repo.ex
@@ -92,4 +92,59 @@ defmodule BetUnfair.Repo do
       _ -> {:error, {:error_delete_bet, "Bet cannot be deleted"}}
     end
   end
+
+  # Market operations
+  # =================
+
+  def get_market(id) do
+    case Models.Market |> BetUnfair.Repo.get_by(id: id) do
+      nil -> {:ok, %{}}
+      market -> {:ok, market}
+    end
+  end
+
+  def add_market(market) do
+    case BetUnfair.Repo.insert(market) do
+      {:ok, _} = res -> res
+      _ -> {:error, {:error_add_market, "Market cannot be inserted in DB"}}
+    end
+  end
+
+  def edit_market(market) do
+    case BetUnfair.Repo.update(market) do
+      {:ok, _} = res -> res
+      _ -> {:error, {:error_edit_market, "Market cannot be edited in the DB"}}
+    end
+  end
+
+  def get_all_markets() do
+    {:ok, Models.Market |> BetUnfair.Repo.all()}
+  end
+
+  def get_status_markets(status) do
+    # Given a status returns all markets that match that status
+    {:ok,
+     from(m in Models.Market, where: m.status == ^status)
+     |> BetUnfair.Repo.all()}
+  end
+
+  def get_market_bets(id) do
+    # Given a market ID return all its bets
+    {:ok,
+     from(b in Models.Bet, where: b.market == ^id, order_by: b.odds)
+     |> BetUnfair.Repo.all()}
+  end
+
+  # WARNING: THIS METHOD IS COMPLETELY MADE UP. USE UNDER YOUR OWN RISK
+  def get_market_pending_bets(id, type) do
+    # Given market ID and bet type, get all pendings bets in the market that match the type
+    {:ok,
+     from(b in Models.Bet,
+       where: b.market == ^id,
+       where: b.bet_type == ^type,
+       where: b.remaining_stake != 0,
+       order_by: b.odds
+     )
+     |> BetUnfair.Repo.all()}
+  end
 end
